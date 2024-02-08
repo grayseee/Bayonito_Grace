15879b65c8974ec0e59a3ee7a1505d2508b509e4
bayonito.290570 <bayonito.290570@LAB-A-PC25>
2024-02-08 08:34:10 +0800

    Add project files.

---
+++ Bayonito_Grace/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Bayonito_Grace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+
+            Console.Write("Enter total of your Enrolled courses: ");
+            int course;
+            course = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the price of your favorite book:");
+            double price;
+            price = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Hi! Your name is: " + name);
+            Console.WriteLine("your couse is: " + course);
+            Console.WriteLine("price of your my book is: " + price);
+        }
+    }
+}
+       
